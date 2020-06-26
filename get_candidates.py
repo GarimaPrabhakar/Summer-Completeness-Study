cb25fa6e462e6d17c4767c0eb343a760804f6f4f
GarimaPrabhakar <35196513+GarimaPrabhakar@users.noreply.github.com>
2020-06-25 21:43:26 -0400

    first commit

---
+++ get_candidates.py
@@ -0,0 +1,26 @@
+import numpy as np
+import pandas as pd
+import random
+
+
+df = pd.read_csv("Trifonov bank.csv")
+
+df_Gstars = pd.DataFrame()
+df_sample = pd.DataFrame()
+
+for i in range(len(df.index)):
+    if "G" in str(df["Sp. Type"].iloc[i]):
+        df_Gstars = df_Gstars.append(df.iloc[i])
+
+df_Gstars.to_csv("G-Type Stars in Trifonov Bank")
+
+sample_list = [random.randint(0, len(df_Gstars.index)) for x in range(50)]
+
+df_sample = pd.concat([pd.DataFrame([df_Gstars.iloc[i]]) for i in sample_list],
+          ignore_index=True)
+
+print(df_sample)
+
+df_sample.to_csv("Sample set from Trifonov Bank")
+
+
